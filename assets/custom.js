73898653eeb1c40f5425f67ce59adb4495224072
crea8ive <crea8ivedev@gmail.com>
2025-07-23 19:15:42 +0530

    refactor: homepage

---
+++ assets/custom.js
@@ -0,0 +1,64 @@
+// product slider
+document.addEventListener("DOMContentLoaded", function () {
+  const swiperThumbs = new Swiper(".ctm_pdp_mainproduct_thumbnail_slider", {
+    loop: true,
+    spaceBetween: 39,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      0: {
+        spaceBetween: 12,
+      },
+      1440: {
+        spaceBetween: 39,
+      },
+    },
+  });
+
+  const swiperMain = new Swiper(".ctm_pdp_mainproduct_slider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    draggable: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiperThumbs,
+    },
+    breakpoints: {
+      0: {
+        spaceBetween: 2,
+      },
+      768: {
+        spaceBetween: 6,
+      },
+    },
+  });
+});
+// Featured Collection
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".mySwiper", {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
